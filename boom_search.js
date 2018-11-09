fb6efefa7fda97ac1fa4c6e7ab969548f1a0551d
sklee01 <sklee01@simplexi.com>
2018-11-09 19:31:00 +0900

    버그 수정... 성능개선좀하자...

--- boom_search.js
+++ boom_search.js
@@ -10,18 +10,20 @@ var font_color = ["blue","green","red", "yello","pink"];
 //status : -1 지뢰, 1 : 미오픈 , 2: 오픈 , 4: 깃발, 3:물음표
 function getNearbyMineCount(row_index, col_index, hor, ver) {
     ++check_count;
-    var row_start_index = row_index - 1 < 0 ? row_index : row_index - 1;
-    var row_end_index = row_index + 1 > hor - 1 ? row_index : row_index + 1;
-    var col_start_index = col_index - 1 < 0 ? col_index : col_index - 1;
-    var col_end_index = col_index + 1 > ver - 1 ? col_index : col_index + 1;
+    console.log(`hor : ${hor}, ver : ${ver}`);
+    console.log(`row : ${row_index}, col : ${col_index}`);
+
+    var row_start_index = (row_index - 1) < 0 ? row_index : (row_index - 1);
+    var row_end_index = (row_index + 1) > hor -1? row_index : (row_index + 1);
+    var col_start_index = (col_index - 1 )< 0 ? col_index : (col_index - 1);
+    var col_end_index = (col_index + 1 )> ver -1? col_index : (col_index + 1);
     var mine_count = 0;
     var search_list = [];
 
-    console.log(row_start_index);
-    console.log(row_end_index);
 
-    console.log(col_start_index);
-    console.log(col_end_index);
+    console.log(`row_start_index : ${row_start_index}, row_end_index : ${row_end_index}`);
+    console.log(`col_start_index : ${col_start_index}, col_end_index : ${col_end_index}`);
+    check_history.push(row_index+":"+col_index);
 
     for (var row = row_start_index; row <= row_end_index; row += 1) {
         for (var col = col_start_index; col <= col_end_index; col += 1) {
@@ -34,7 +36,7 @@ function getNearbyMineCount(row_index, col_index, hor, ver) {
                 mine_count += 1;
             } else {
                 //&& [1,2].indexOf(dataset[row][col].flag) === -1
-                if (dataset[row][col].status !== 2 ) {
+                if (dataset[row][col].status !== 2 && check_history.indexOf(row+":"+col) === -1) {
                     search_list.push({row: row, col: col, });
                     // if (check_history.indexOf(row+":"+col) === -1) {
                     //     search_list.push({row: row, col: col, });
@@ -52,18 +54,19 @@ function getNearbyMineCount(row_index, col_index, hor, ver) {
         openMine(row_index, col_index, mine_count);
     }
 
-    console.log('search_list ' , search_list);
+
     if (mine_count === 0 ) {
         //search_list = search_list.concat(new_search_list);
         //console.log(search_list);
         //클릭한 폭탄이 0이면 주변 을 다시 검사한다..
+        console.log('search_list ' , search_list);
         for (var i=0; i< search_list.length; i++) {
             //openMine(search_list[i].row, search_list[i].col, mine_count);
             var ser_row = search_list[i].row;
             var ser_col = search_list[i].col;
             //search_list.splice(i,1);
 
-            getNearbyMineCount(ser_row, ser_col, hor, ver);
+            getNearbyMineCount(parseInt(ser_row), parseInt(ser_col), hor, ver);
         }
     }
     // else {
@@ -126,8 +129,8 @@ document.querySelector('#exec').addEventListener('click', function (e) {
             td.dataset.col = j;
             td.addEventListener('click', function(e){
                 var target_data = e.currentTarget.dataset;
-                var row_index = target_data.row;
-                var col_index = target_data.col;
+                var row_index = parseInt(target_data.row);
+                var col_index = parseInt(target_data.col);
 
                 if(dataset[row_index][col_index].status === -1) {
                     e.currentTarget.textContent = 'X';
@@ -143,8 +146,8 @@ document.querySelector('#exec').addEventListener('click', function (e) {
 
             td.addEventListener('contextmenu', function(e) {
                 var target_data = e.currentTarget.dataset;
-                var row_index = target_data.row;
-                var col_index = target_data.col;
+                var row_index = parseInt(target_data.row);
+                var col_index = parseInt(target_data.col);
                 var target_status = dataset[row_index][col_index];
                 e.preventDefault();
                 if (flag_count === 0 && [1,2].indexOf(target_status.flag) === -1) {
@@ -175,7 +178,9 @@ document.querySelector('#exec').addEventListener('click', function (e) {
 
     //shuffle = [315, 283, 195, 53, 285, 314, 73, 99, 163, 349, 336, 210, 342, 245, 322, 251, 228, 152, 75, 184];
     //shuffle = [581, 448, 463, 63, 48, 170, 618, 503, 32, 483, 452, 64, 236, 617, 30, 94, 324, 340, 95, 133, 266, 268, 23, 54, 214, 584, 539, 317, 360, 207, 253, 371, 125, 370, 377, 283, 131, 280, 589, 358, 194, 134, 293, 123, 613, 138, 575, 122, 354, 596];
-    shuffle =  [75, 40, 87, 19, 3, 13, 85, 17, 64, 80, 14, 88, 4, 27, 28, 29, 74, 32, 10, 58];
+    //shuffle =  [75, 40, 87, 19, 3, 13, 85, 17, 64, 80, 14, 88, 4, 27, 28, 29, 74, 32, 10, 58];
+    //shuffle = [32, 348, 298, 2, 378, 204, 345, 363, 239, 12, 330, 178, 333, 66, 160, 280, 89, 273, 79, 344, 181, 385, 307, 134, 191, 206, 116, 387, 27, 76, 44, 201, 132, 393, 309, 224, 144, 30, 244, 39, 281, 62, 301, 279, 63, 35, 338, 101, 386, 394, 261, 221, 153, 61, 37, 105, 389, 88, 235, 311, 243, 143, 49, 82, 240, 84, 147, 381, 373, 198, 23, 83, 220, 71, 339, 183, 323, 196, 200, 283];
+    shuffle = [606, 1104, 1114, 524, 1225, 432, 1113, 1001, 1047, 279, 1184, 695, 347, 932, 1325, 356, 1596, 1247, 1009, 502, 1073, 9, 438, 929, 594, 1425, 1197, 851, 1211, 1575, 99, 1388, 993, 1155, 1012, 718, 471, 895, 939, 142, 385, 45, 1025, 671, 1017, 1371, 640, 1213, 298, 203, 95, 154, 417, 1344, 490, 23, 971, 1378, 433, 1288];
     //지뢰심기
     for (var i = 0; i < shuffle.length ; i++) {
         var row = Math.floor(shuffle[i] / ver);
